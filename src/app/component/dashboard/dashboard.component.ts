42f3392095abbf046f342a138abe3aca57dccdca
It21217654 <IT21217654@my.sliit.lk>
2024-01-10 00:49:28 +0530

    initial commit

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
+  selector: 'app-dashboard', // Make sure the selector is 'app-dashboard'
+  styleUrls: ['./dashboard.component.css'],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
