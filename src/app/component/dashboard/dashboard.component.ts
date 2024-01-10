49d38dc04e1b5e7b651810087f9def8760909376
It21217654 <IT21217654@my.sliit.lk>
2024-01-11 01:27:48 +0530

    intial commit

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/service/crud.service';
 
 @Component({
-  selector: 'app-dashboard', // Make sure the selector is 'app-dashboard'
-  styleUrls: ['./dashboard.component.css'],
+  selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-}
+  taskObj : Task = new Task();
+  taskArr : Task[] = [];
+
+  addTaskValue : string = '';
+  editTaskValue : string = '';
+
+  constructor(private crudService : CrudService) { }
+
+  ngOnInit(): void {
+    this.editTaskValue = '';
+    this.addTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(res => {
+      this.taskArr = res;
+    }, err => {
+      alert("Unable to get list of tasks");
+    });
+  }
+
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(res => {
+      this.ngOnInit();
+      this.addTaskValue = '';
+    }, err => {
+      alert(err);
+    })
+  }
