4177d4673b9f9541dd8185dac0f7647427d3de71
It21217654 <IT21217654@my.sliit.lk>
2024-01-11 01:03:30 +0530

    intial commit

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../model/task';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,13 @@ export class CrudService {
 
   constructor(private http : HttpClient) { 
     this.serviceURL="http://localhost:3000/tasks"
+  }
+  addTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.serviceURL,task);
+
+  }
+  getAllTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.serviceURL,task);
+
   }
 }
